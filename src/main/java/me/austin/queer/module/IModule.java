f2ba36eb5278a4bdbec088fa83849c3cf5e52dad
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2021-11-11 21:07:45 -0700

    Delete IModule.java

--- src/main/java/me/austin/queer/module/IModule.java
+++
@@ -1,9 +0,0 @@
-package me.austin.queer.module;
-/**
- * @author Austin
- * meant to make this client modular
- */
-public interface IModule {
-    String getName();
-    String getDescription();
-}
