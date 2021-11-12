539eedc274440899e994dcebb3481760d1a4b184
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2021-11-11 23:52:09 -0700

    Delete src/main/java/me/austin/queer/module directory

--- src/main/java/me/austin/queer/module/gui/hud/HudComponent.java
+++
@@ -1,9 +0,0 @@
-package me.austin.queer.module.gui.hud;
-
-import me.austin.queer.module.gui.Component;
-
-public abstract class HudComponent extends Component {
-    public HudComponent(String name, String description, int x, int y, int width, int height) {
-        super(name, description, x, y, width, height);
-    }
-}
