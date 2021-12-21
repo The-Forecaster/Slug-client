8915e92d8d30d23a7f2d5528ee1301508c745420
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2021-12-20 20:18:27 -0700

    Delete src/main/java/me/austin/queer/event directory

--- src/main/java/me/austin/queer/event/events/TickEvent.java
+++
@@ -1,6 +0,0 @@
-package me.austin.queer.event.events;
-
-import me.austin.queer.event.Event;
-
-public class TickEvent extends Event {
-}
--- src/main/java/me/austin/queer/event/system/TimeEvent.java
+++
@@ -1,7 +0,0 @@
-package me.austin.queer.event.system;
-
-import me.austin.queer.event.Event;
-
-public class TimeEvent extends Event {
-    
-}
