2940af9d976230bea15b4927b1a29a296cdf6128
JacobHerd <toxicaven@disroot.org>
2021-12-24 23:02:35 -0500

    Remove use of MC Utils when they aren't available

--- src/main/java/me/austin/queer/Main.java
+++ src/main/java/me/austin/queer/Main.java
@@ -1,28 +1,51 @@
 package me.austin.queer;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Locale;
 
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import net.minecraft.util.Util;
-
 public class Main {
     public static final ImageIcon icon = new ImageIcon("src/main/resources/assets/transrights/transpride.png");
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException, IOException {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
-        if (JOptionPane.showConfirmDialog(null, "Don't run this file, put it in your mods folder!\nWould you like to open up your mods folder?", "ERROR", 0, 0, icon) == 0) {
-            var modsFile = switch (Util.getOperatingSystem()) {
+        if (JOptionPane.showConfirmDialog(null, "Don't run this file, put it in your mods folder!\nWould you like to open up your mods folder?", "ERROR", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE, icon) == 0) {
+            var modsFile = switch (getOS()) {
                 case WINDOWS -> new File(System.getenv("AppData") + "/.minecraft/mods");
                 case OSX -> new File(System.getProperty("user.home") + "/Library/Application Support/minecraft/mods");
-                default -> new File(System.getProperty("user.home") + "/.minecraft");
+                case NIX -> new File(System.getProperty("user.home") + "/.minecraft");
+                default -> throw new IllegalStateException("Unknown OS: " + getOS());
             };
 
-            if (modsFile.exists()) modsFile.mkdirs();
-            Util.getOperatingSystem().open(modsFile);
+            if (!modsFile.exists()) modsFile.mkdirs();
+            Runtime.getRuntime().exec(getURLOpenCommand(modsFile.toURI().toURL()));
         }
+    }
+
+    private static OS getOS() {
+        String osName = System.getProperty("os.name").toLowerCase(Locale.ROOT);
+        if (osName.contains("nux") || osName.contains("nix")) return OS.NIX;
+        else if (osName.contains("darwin") || osName.contains("mac")) return OS.OSX;
+        else if (osName.contains("win")) return OS.WINDOWS;
+        else return OS.UNKNOWN;
+    }
+
+    private static String[] getURLOpenCommand(URL url) {
+        String string = url.toString();
+        if ("file".equals(url.getProtocol())) {
+            string = string.replace("file:", "file://");
+        }
+
+        return new String[]{"xdg-open", string};
+    }
+
+    enum OS {
+        WINDOWS, OSX, NIX, UNKNOWN
     }
 }
