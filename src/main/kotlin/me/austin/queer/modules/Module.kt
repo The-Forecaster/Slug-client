a5151490d57a67f61264eb4325cd35eb9d5cb825
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-01-22 19:46:10 -0700

    Delete src/main/kotlin/me/austin/queer directory

--- src/main/kotlin/me/austin/queer/modules/Module.kt
+++
@@ -1,3 +0,0 @@
-package me.austin.queer.modules
-
-abstract class Module(val name: String, val description: String)
--- src/main/kotlin/me/austin/queer/modules/hack/hacks/AutoHit.kt
+++
@@ -1,5 +0,0 @@
-package me.austin.queer.modules.hack.hacks
-
-import me.austin.queer.modules.hack.Hack
-
-object AutoHit : Hack("Auto-hit", "Automatically hit people near you") {}
