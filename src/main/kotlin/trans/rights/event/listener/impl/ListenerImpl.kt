f0da3b6c0aa4d3a52b352577058a458d37fee498
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-03-30 23:33:00 -0600

    Update ListenerImpl.kt

--- src/main/kotlin/trans/rights/event/listener/impl/ListenerImpl.kt
+++ src/main/kotlin/trans/rights/event/listener/impl/ListenerImpl.kt
@@ -6,10 +6,7 @@ import java.lang.reflect.Method
 
 /** Implementation of Listener that uses a method as its target */
 class MethodListener<T : Any>(
-    private val action: Method,
-    override val priority: Int,
-    override val parent: Any,
-    override val target: Class<T>
+    private val action: Method, override val priority: Int, override val parent: Any, override val target: Class<T>
 ) : Listener<T> {
     override operator fun invoke(param: T) {
         this.action(this.parent, param)
@@ -21,27 +18,20 @@ inline fun <reified T : Any> Any.lambdaListener(noinline action: (T) -> Unit): L
 }
 
 inline fun <reified T : Any> Any.lambdaListener(
-    noinline action: (T) -> Unit,
-    priority: Int
+    noinline action: (T) -> Unit, priority: Int
 ): LambdaListener<T> {
     return listener(action, priority, this, T::class.java)
 }
 
 fun <T : Any> listener(
-    action: (T) -> Unit,
-    priority: Int,
-    parent: Any,
-    target: Class<T>
+    action: (T) -> Unit, priority: Int, parent: Any, target: Class<T>
 ): LambdaListener<T> {
     return LambdaListener(action, priority, parent, target)
 }
 
 /** Implementation of Listener that uses a lambda function as its target */
 class LambdaListener<T : Any>(
-    private val action: (T) -> Unit,
-    override val priority: Int,
-    override val parent: Any,
-    override val target: Class<T>
+    private val action: (T) -> Unit, override val priority: Int, override val parent: Any, override val target: Class<T>
 ) : Listener<T> {
     override operator fun invoke(param: T) {
         this.action(param)
