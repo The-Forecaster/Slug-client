1d7538fb0eb4a887e6ed1a462e987c01a9333eed
Austin the Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2023-01-07 13:45:00 -0700

    Update AbstractBlockMixin.java

--- src/main/java/trans/rights/mixin/AbstractBlockMixin.java
+++ src/main/java/trans/rights/mixin/AbstractBlockMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import trans.rights.impl.hack.Wallhack;
+import trans.rights.impl.hack.WallHack;
 
 @Mixin(AbstractBlock.class)
 public class AbstractBlockMixin {
