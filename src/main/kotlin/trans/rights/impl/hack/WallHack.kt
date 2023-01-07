d554a71e5e873621c9cc4efacc01fe4d542a9aeb
Austin the Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2023-01-07 12:51:46 -0700

    Update WallHack.kt

--- src/main/kotlin/trans/rights/impl/hack/WallHack.kt
+++ src/main/kotlin/trans/rights/impl/hack/WallHack.kt
@@ -5,6 +5,10 @@ import net.minecraft.block.Blocks
 import trans.rights.api.hack.Hack
 
 object WallHack : Hack("Wallhack", "Makes blocks see through and highlights players") {
+    enum class Opacity {
+        SOME, NONE
+    }
+    
     private val blocks = mutableSetOf(Blocks.DIAMOND_ORE, Blocks.ANCIENT_DEBRIS, Blocks.ENDER_CHEST, Blocks.BEDROCK)
 
     fun shouldRender(block: Block) = blocks.contains(block)
