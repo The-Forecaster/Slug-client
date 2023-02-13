2a179fb6ec3224bab9187a22836f6a07c165c381
The-Forecaster <DarkDruid11@gmail.com>
2023-02-13 09:42:22 -0700

    renamed main package

--- src/main/kotlin/me/austin/slug/api/gui/components/buttons/Button.kt
+++
@@ -1,5 +0,0 @@
-package me.austin.client.api.gui.components.buttons
-
-import me.austin.client.api.gui.Component
-
-abstract class Button<T>(xPos: Int, yPos: Int, width: Int, height: Int, private val parent: T) : Component(xPos, yPos, width, height)
--- src/main/kotlin/me/austin/slug/impl/setting/BooleanSetting.kt
+++
@@ -1,7 +0,0 @@
-package me.austin.client.impl.setting
-
-import me.austin.client.api.setting.Setting
-
-class BooleanSetting(
-    name: String, description: String, default: Boolean, vararg children: Setting<*>
-) : Setting<Boolean>(name, description, default, *children)
