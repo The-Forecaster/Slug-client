3fc5ee75b8fef06ff0cb69f569435f93366a8c8a
The-Forecaster <DarkDruid11@gmail.com>
2023-03-06 12:37:54 -0700

    fixed my fuckup

--- src/main/kotlin/me/austin/client/impl/command/arguments/ArgumentTypes.kt
+++ src/main/kotlin/me/austin/client/impl/command/arguments/ArgumentTypes.kt
@@ -4,19 +4,18 @@ import com.mojang.brigadier.StringReader
 import com.mojang.brigadier.arguments.ArgumentType
 import com.mojang.brigadier.context.CommandContext
 import com.mojang.brigadier.exceptions.CommandSyntaxException
+import com.mojang.brigadier.exceptions.BuiltInExceptions
 import com.mojang.brigadier.suggestion.Suggestions
 import com.mojang.brigadier.suggestion.SuggestionsBuilder
 import net.minecraft.command.CommandSource
 import me.austin.client.api.hack.Hack
+import me.austin.client.api.setting.Setting
 import me.austin.client.api.hack.HackManager
 import java.util.concurrent.CompletableFuture
 
 class HackArgumentType internal constructor() : ArgumentType<Hack> {
     @Throws(CommandSyntaxException::class)
-    override fun parse(reader: StringReader): Hack? {
-        for (hack in HackManager.values) if (reader.readString().lowercase() == hack.name.lowercase()) return hack
-        return null
-    }
+    override fun parse(reader: StringReader): Hack? = HackManager.values.find { reader.readString().lowercase() == it.name.lowercase() }
 
     override fun <S : Any> listSuggestions(
         context: CommandContext<S>?, builder: SuggestionsBuilder?
