acb0cade4a50c397baecd868dc88ea190250133e
The-Forecaster <DarkDruid11@gmail.com>
2023-03-08 09:47:41 -0700

    updated ClientConnectionMixin.java

--- src/main/java/me/austin/client/mixin/ClientConnectionMixin.java
+++ src/main/java/me/austin/client/mixin/ClientConnectionMixin.java
@@ -19,17 +19,14 @@ import net.minecraft.network.listener.PacketListener;
 public class ClientConnectionMixin {
     @Inject(method = "handlePacket", at = @At("TAIL"), cancellable = true)
     private static void afterRead(Packet<?> packet, PacketListener listener, CallbackInfo info) {
-        postCancel(new PacketEvent.PostReceive(packet), info);
-    }
-
-    private static void postCancel(PacketEvent event, CallbackInfo info) {
-        if (BasicEventManager.INSTANCE.dispatch(event).isCancelled())
+        if (BasicEventManager.INSTANCE.dispatch(new PacketEvent.PostReceive(packet)).isCancelled())
             info.cancel();
     }
 
     @Inject(method = "send", at = @At("HEAD"), cancellable = true)
     private void beforeSend(Packet<?> packet, CallbackInfo info) {
-        postCancel(new PacketEvent.PreSend(packet), info);
+        if (BasicEventManager.INSTANCE.dispatch(new PacketEvent.PreSend(packet)).isCancelled())
+            info.cancel();
     }
 
     @Inject(method = "exceptionCaught", at = @At("HEAD"), cancellable = true)
